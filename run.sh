b39dc37e03956402afbd30a6c5d651ae8afd87f3
Juan A. Garcia Pardo <juagargi@gmail.com>
2025-08-22 15:00:52 +0200

    Modify run script.

--- run.sh
+++ run.sh
@@ -9,12 +9,12 @@ set -x
 # # Update tools, sysroots, etc.
 # cd .. && gclient runhooks
 
-# Generate project:
-gn gen out/Gentoo --args='is_debug=true is_component_build=true'
+# # Generate project:
+# gn gen out/Gentoo --args='is_debug=true is_component_build=true'
 
 # Build:
-# time autoninja -C out/Gentoo/ -j16
-time autoninja -C out/Gentoo/ chrome
+time autoninja -j16 -C out/Gentoo/ chrome
+# time autoninja -C out/Gentoo/ chrome
 
 # Run:
 # ./out/Gentoo/chrome
