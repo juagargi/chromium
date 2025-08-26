53adcc30116ccbdf52010f22bb041c183cc7d7cc
Juan A. Garcia Pardo <juagargi@gmail.com>
2025-08-26 17:31:25 +0200

    Update build/run host script.

--- run.sh
+++ run.sh
@@ -9,12 +9,17 @@ set -x
 # # Update tools, sysroots, etc.
 # cd .. && gclient runhooks
 
-# # Generate project:
+# Clean: ( DO NOT CLEAN!! )
+#gn clean out/Gentoo
+
+# Generate project:
 # gn gen out/Gentoo --args='is_debug=true is_component_build=true'
+gn gen out/Gentoo --args='is_debug=true is_component_build=true use_lld=true'
 
 # Build:
-time autoninja -j16 -C out/Gentoo/ chrome
-# time autoninja -C out/Gentoo/ chrome
+# time autoninja -j16 -C out/Gentoo/ chrome
+time autoninja -C out/Gentoo/ chrome
 
 # Run:
-# ./out/Gentoo/chrome
+# clear && ./out/Gentoo/chrome --user-data-dir=/tmp/chrome-dev --enable-logging=stderr --v=1
+clear && ./out/Gentoo/chrome --user-data-dir=/tmp/chrome-dev
